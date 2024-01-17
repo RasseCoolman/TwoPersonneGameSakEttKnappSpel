05414be4e7df6c312bc6b7d7fe2f4eb264046bcb
Successyy <146823522+Successyy@users.noreply.github.com>
2024-01-17 13:51:38 +0100

    movement done almost in fortnite

--- hentai explorers 31/Assets/Move.cs
+++ hentai explorers 31/Assets/Move.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
 
